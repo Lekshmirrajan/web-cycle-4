ccce95387ea5328f170195b7edec9d11ca0beb8c
LEKSHMI R RAJAN <lekshmirajan2345@gmail.com>
2024-12-12 11:10:02 +0530

    Create exp.php

---
+++ exp.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Information Form</title>
+</head>
+<body>
+    <form action="4_1.php" method="post">
+        ID: <input type="text" name="id"><br><br>
+        Name: <input type="text" name="name"><br><br>
+        Age: <input type="text" name="age"><br><br> <!-- Corrected 'edges' to 'age' -->
+        Class: <input type="text" name="class"><br><br>
+
+        <input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+    </form>
+</body>
+</html>
